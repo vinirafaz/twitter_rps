c1d70fae7824bf5486cdcc8f7a24882a65910b0d
vinirafaz <vinirafa0306@gmail.com>
2023-02-16 21:57:00 -0300

    Special message

---
+++ jokenpo.py
@@ -0,0 +1,80 @@
+import names
+import random
+import emoji
+
+
+class Player:
+    def __init__(self):
+        self.__name = names.get_first_name()
+        self.__jokenpo_move, self.__emoji_result = self.__move()
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def jokenpo_move(self):
+        return self.__jokenpo_move
+
+    @property
+    def emoji_result(self):
+        return self.__emoji_result
+
+    @staticmethod
+    def __move():
+        play = random.choice(['rock', 'paper', 'scissor'])
+
+        if play == 'rock':
+            emoji_result = emoji.emojize(':fist:', language='alias')
+            return play, emoji_result
+        elif play == 'paper':
+            emoji_result = emoji.emojize(':hand:', language='alias')
+            return play, emoji_result
+        elif play == 'scissor':
+            emoji_result = emoji.emojize(':v:', language='alias')
+            return play, emoji_result
+
+
+class Jokenpo:
+    def __init__(self):
+        self.__player1 = Player()
+        self.__player2 = Player()
+        self.__result_text = self.__result()
+
+    @property
+    def result_text(self):
+        return self.__result_text
+
+    def __rules(self):
+        if self.__player1.jokenpo_move == self.__player2.jokenpo_move:
+            return 'Draw'
+        elif (self.__player1.jokenpo_move == 'rock' and self.__player2.jokenpo_move == 'scissor') or \
+                (self.__player1.jokenpo_move == 'paper' and self.__player2.jokenpo_move == 'rock') \
+                or (self.__player1.jokenpo_move == 'scissor' and self.__player2.jokenpo_move == 'paper'):
+            return 'p_one'
+        else:
+            return 'p_two'
+
+    def __result(self):
+        if self.__rules() == 'p_one':
+            result = f'{self.__player1.name} is a winner'
+        elif self.__rules() == 'p_two':
+            result = f'{self.__player2.name} is a winner'
+        else:
+            result = 'The players drew'
+
+        return f"{self.__player1.name} {self.__player1.emoji_result} X {self.__player2.emoji_result} " \
+               f"{self.__player2.name}\n" \
+               f"{result}"
+
+
+# TO Develop menu to play on terminal and vs human
+def menu():
+    pass
+
+
+if __name__ == '__main__':
+    menu()
+
+    jokenpo = Jokenpo()
+    print(jokenpo.result_text)
---
+++ main.py
@@ -0,0 +1,37 @@
+import tweepy
+import os
+from dotenv import load_dotenv
+from jokenpo import Jokenpo
+
+
+# Isolate env variables
+load_dotenv()
+CONSUMER_KEY_TT = os.getenv('CONSUMER_KEY_TT')
+CONSUMER_SECRET_TT = os.getenv('CONSUMER_SECRET_TT')
+ACCESS_TOKEN_TT = os.getenv('ACCESS_TOKEN_TT')
+ACESS_TOKEN_SECRET_TT = os.getenv('ACESS_TOKEN_SECRET_TT')
+
+
+#teste
+# Authenticate to Twitter
+def authentication_tt():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY_TT, CONSUMER_SECRET_TT)
+    auth.set_access_token(ACCESS_TOKEN_TT, ACESS_TOKEN_SECRET_TT)
+    return tweepy.API(auth)
+
+
+if __name__ == '__main__':
+    tweet = Jokenpo().result_text
+
+    api = authentication_tt()
+
+    api.update_status(tweet)
+
+    # try:
+    #
+    # except tweepy.TweepyException as error:
+    #     if error == 187:
+    #         # Do something special
+    #         print('duplicate message')
+    #     else:
+    #         raise error
